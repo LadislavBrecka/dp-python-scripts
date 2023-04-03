553f1553e2ec3d42881b186936b39a80f2a70970
LadislavBrecka <lacoobrecka@gmail.com>
2023-04-03 18:43:50 +0200

    Add new data plot

--- data_reader.py
+++ data_reader.py
@@ -13,10 +13,11 @@ import sys
 from itertools import repeat
 
 class UsartData:
-    def __init__(self, sp, speed, abs_pos):
-        self.sp = sp
-        self.speed = speed
-        self.abs_pos = abs_pos
+    def __init__(self, u_speed, y_speed, w_pos, y_pos):
+        self.u_speed = u_speed
+        self.y_speed = y_speed
+        self.w_pos = w_pos
+        self.y_pos = y_pos
 
 
 class serialPlot:
@@ -32,10 +33,11 @@ class serialPlot:
         self.dataNumBytes = dataNumBytes
         self.rawData = bytes
 
-        self.data_sp = collections.deque([0] * plotLength, maxlen=plotLength)
-        self.data_speed = collections.deque([0] * plotLength, maxlen=plotLength)
-        self.data_abs_pos = collections.deque([0] * plotLength, maxlen=plotLength)
-        self.abs_pos_ylim = (-10000, 10000)
+        self.data_u_speed = collections.deque([0] * plotLength, maxlen=plotLength)
+        self.data_y_speed = collections.deque([0] * plotLength, maxlen=plotLength)
+        self.data_w_pos = collections.deque([0] * plotLength, maxlen=plotLength)
+        self.data_y_pos = collections.deque([0] * plotLength, maxlen=plotLength)
+        self.abs_pos_ylim = (-1000, 1000)
         # self.csvData = []
 
         print('Trying to connect to: ' + str(serialPort) +
@@ -58,9 +60,10 @@ class serialPlot:
                 time.sleep(0.1)
 
     def updateGraphs(self, frame, timeText,
-                      lines_sp, lineValueText_sp, lineLabel_sp, ax1,
-                      lines_speed, lineValueText_speed, lineLabel_speed, ax2,
-                      lines_abs_pos, lineValueText_abs_pos, lineLabel_abs_pos, ax3):
+                      lines_u_speed, lineValueText_u_speed, lineLabel_u_speed, ax1,
+                      lines_y_speed, lineValueText_y_speed, lineLabel_y_speed, ax2,
+                      lines_w_pos, lineValueText_w_pos, lineLabel_w_pos, ax3,
+                      lines_y_pos, lineValueText_y_pos, lineLabel_y_pos, ax4):
 
         currentTimer = time.perf_counter()
         # the first reading will be erroneous
@@ -71,34 +74,42 @@ class serialPlot:
 
         # we get the latest data point and append it to our array
         try:
-            new_values = struct.unpack('<iii', self.rawData)  # unpack the values
+            new_values = struct.unpack('<iiii', self.rawData)  # unpack the values
             parsed_data = UsartData(*new_values)
             print(parsed_data)
         except Exception:
             print(self.rawData)
-            parsed_data = UsartData(self.data_sp[-1],
-                                    self.data_speed[-1],
-                                    self.data_abs_pos[-1])
+            parsed_data = UsartData(self.data_u_speed[-1],
+                                    self.data_y_speed[-1],
+                                    self.data_w_pos[-1],
+                                    self.data_y_pos[-1])
 
-        self.data_sp.append(parsed_data.sp)
-        self.data_speed.append(parsed_data.speed)
-        self.data_abs_pos.append(parsed_data.abs_pos)
+        self.data_u_speed.append(parsed_data.u_speed)
+        self.data_y_speed.append(parsed_data.y_speed)
+        self.data_w_pos.append(parsed_data.w_pos)
+        self.data_y_pos.append(parsed_data.y_pos)
 
-        lines_sp.set_data(range(self.plotMaxLength), self.data_sp)
-        lines_speed.set_data(range(self.plotMaxLength), self.data_speed)
-        lines_abs_pos.set_data(range(self.plotMaxLength), self.data_abs_pos)
+        lines_u_speed.set_data(range(self.plotMaxLength), self.data_u_speed)
+        lines_y_speed.set_data(range(self.plotMaxLength), self.data_y_speed)
+        lines_w_pos.set_data(range(self.plotMaxLength), self.data_w_pos)
+        lines_y_pos.set_data(range(self.plotMaxLength), self.data_y_pos)
 
-        lineValueText_sp.set_text('[' + lineLabel_sp + '] = ' + str(parsed_data.sp))
-        lineValueText_speed.set_text('[' + lineLabel_speed + '] = ' + str(parsed_data.speed))
-        lineValueText_abs_pos.set_text('[' + lineLabel_abs_pos + '] = ' + str(parsed_data.abs_pos))
+        lineValueText_u_speed.set_text('[' + lineLabel_u_speed + '] = ' + str(parsed_data.u_speed))
+        lineValueText_y_speed.set_text('[' + lineLabel_y_speed + '] = ' + str(parsed_data.y_speed))
+        lineValueText_w_pos.set_text('[' + lineLabel_w_pos + '] = ' + str(parsed_data.w_pos))
+        lineValueText_y_pos.set_text('[' + lineLabel_y_pos + '] = ' + str(parsed_data.y_pos))
 
-        # dynamically adjust axes 3 if abs pos. overflow y limit
-        if (abs(parsed_data.abs_pos) > self.abs_pos_ylim[1]):
+        # dynamically adjust ax3 if abs pos. overflow y limit
+        if (abs(parsed_data.y_pos) > self.abs_pos_ylim[1] or abs(parsed_data.w_pos) > self.abs_pos_ylim[1]):
             self.abs_pos_ylim = (self.abs_pos_ylim[0] * 2, self.abs_pos_ylim[1] * 2)
             ax3.set_ylim(self.abs_pos_ylim[0], self.abs_pos_ylim[1])
-        elif (max(abs(min(self.data_abs_pos)), abs(max(self.data_abs_pos))) < self.abs_pos_ylim[1] / 2 and self.abs_pos_ylim[1] > 10000):
+            ax4.set_ylim(self.abs_pos_ylim[0], self.abs_pos_ylim[1])
+
+        elif ((max(abs(min(self.data_y_pos)), abs(max(self.data_y_pos))) < self.abs_pos_ylim[1] / 2 and self.abs_pos_ylim[1] > 10000) or 
+              (max(abs(min(self.data_w_pos)), abs(max(self.data_w_pos))) < self.abs_pos_ylim[1] / 2 and self.abs_pos_ylim[1] > 10000)):
             self.abs_pos_ylim = (self.abs_pos_ylim[0] / 2, self.abs_pos_ylim[1] / 2)
             ax3.set_ylim(self.abs_pos_ylim[0], self.abs_pos_ylim[1])
+            ax4.set_ylim(self.abs_pos_ylim[0], self.abs_pos_ylim[1])
 
         # self.csvData.append(self.data[-1])
 
@@ -117,14 +128,14 @@ class serialPlot:
         self.thread.join()
         self.serialConnection.close()
         print('Disconnected...')
-        df = pd.DataFrame([self.data_sp, self.data_speed])
-        df.to_csv('/home/ladislav/Desktop/data.csv')
+        # df = pd.DataFrame([self.data_sp, self.data_speed])
+        # df.to_csv('/home/ladislav/Desktop/data.csv')
 
 
 def main():
     portName = '/dev/ttyACM0'
     baudRate = 115200
-    maxPlotLength = 3000
+    maxPlotLength = 1000
     dataNumBytes = 12
     # initializes all required variables
     s = serialPlot(portName, baudRate, maxPlotLength, dataNumBytes)
@@ -135,45 +146,55 @@ def main():
     pltInterval = 10    # Period at which the plot animation updates [ms]
     xmin = 0
     xmax = maxPlotLength
-    fig, (ax1, ax2, ax3) = plt.subplots(3, 1)
+    fig, ax = plt.subplots(2, 2)
+    ax1 = ax[0, 0]
+    ax2 = ax[0, 1]
+    ax3 = ax[1, 0]
+    ax4 = ax[1, 1]
     timeText = ax1.text(-0.06, 1.2, '', transform=ax1.transAxes)
 
     # subplot 1 - Input
-    lineLabel_sp = 'Percentage of input PWM signal'
-    (lines_sp, lineValueText_sp, lineLabel_sp) = configureSubplot(
-        ax1, title='PWM % of input voltage - Set point', xlabel='Sample [k]', ylabel='Percentage [%]',
-        lineLabel=lineLabel_sp, xlim=(xmin, xmax), ylim=(-100, 100))
+    (lines_u_speed, lineValueText_u_speed, lineLabel_u_speed) = configureSubplot(
+        ax1, title='PWM % of input voltage', xlabel='Sample [k]', ylabel='Percentage [%]',
+        lineLabel='PWM - u_speed', xlim=(xmin, xmax), ylim=(-100, 100))
 
     # subplot 2 - HAL 1 Sensor
-    (lines_speed, lineValueText_speed, lineLabel_speed) = configureSubplot(
+    (lines_y_speed, lineValueText_y_speed, lineLabel_y_speed) = configureSubplot(
         ax2, title='Speed by HAL Sensor', xlabel='Sample [k]', ylabel='Frequency [Hz]',
-        lineLabel='Speed', xlim=(xmin, xmax), ylim=(-350, 350))
+        lineLabel='Speed - y_speed', xlim=(xmin, xmax), ylim=(-350, 350))
 
-    # subplot 3 - HAL 2 Sensor
-    (lines_abs_pos, lineValueText_abs_pos, lineLabel_abs_pos) = configureSubplot(
-        ax3, title='Absolute position by HAL Sensor', xlabel='Sample [k]', ylabel='Abs. pos [ticks]',
-        lineLabel='Absolute position', xlim=(xmin, xmax), ylim=(-10000, 10000))
+    # subplot 3 - Set Point value of position
+    (lines_w_pos, lineValueText_w_pos, lineLabel_w_pos) = configureSubplot(
+        ax3, title='Position Set Point', xlabel='Sample [k]', ylabel='Abs. pos [ticks]',
+        lineLabel='Position - w_pos', xlim=(xmin, xmax), ylim=(-10000, 10000))
+    
+    # subplot 4 - HAL 1 absolute position
+    (lines_y_pos, lineValueText_y_pos, lineLabel_y_pos) = configureSubplot(
+        ax4, title='Position by HAL Sensor', xlabel='Sample [k]', ylabel='Abs. pos [ticks]',
+        lineLabel='Position - y_pos', xlim=(xmin, xmax), ylim=(-10000, 10000))
 
     # animation plotting function
     anim = animation.FuncAnimation(fig, 
                                    s.updateGraphs, 
                                    fargs=(timeText,
-                                          lines_sp, lineValueText_sp, lineLabel_sp, ax1,
-                                          lines_speed, lineValueText_speed, lineLabel_speed, ax2,
-                                          lines_abs_pos, lineValueText_abs_pos, lineLabel_abs_pos, ax3),
+                                          lines_u_speed, lineValueText_u_speed, lineLabel_u_speed, ax1,
+                                          lines_y_speed, lineValueText_y_speed, lineLabel_y_speed, ax2,
+                                          lines_w_pos, lineValueText_w_pos, lineLabel_w_pos, ax3,
+                                          lines_y_pos, lineValueText_y_pos, lineLabel_y_pos, ax4),
                                    interval=pltInterval)
 
     ax1.legend(loc="upper left")
     ax2.legend(loc="upper left")
     ax3.legend(loc="upper left")
+    ax4.legend(loc="upper left")
 
-    plt.tight_layout()
+    # plt.tight_layout()
     plt.show()
     s.close()
 
 
 def configureSubplot(ax, title, xlabel, ylabel, lineLabel, xlim, ylim):
-    ax.set_title(title)
+    # ax.set_title(title)
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
     ax.set_xlim(xlim[0], xlim[1])
